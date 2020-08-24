df4c957e5c8a530701a6a9507252fbce012f966c
garyparrot <weedbug666@gmail.com>
2020-08-25 00:51:04 +0800

    refactor(logger): update non production logging style

--- src/logger.ts
+++ src/logger.ts
@@ -1,20 +1,35 @@
 import winston from 'winston';
 import expressWinston from 'express-winston';
+import {isProduction} from './utility/isProduction';
+
+function winstonFormat(label: string) {
+  if (isProduction) return winston.format.json();
+  else
+    return winston.format.combine(
+      winston.format.colorize(),
+      winston.format.label({label: label}),
+      winston.format.timestamp(),
+      winston.format.printf(({level, message, label, timestamp}) => {
+        return `${timestamp} [${label}] ${level}: ${message}`;
+      }),
+      winston.format.metadata()
+    );
+}
 
 export const appLogger = winston.createLogger({
   transports: [new winston.transports.Console()],
-  format: winston.format.json(),
+  format: winstonFormat('rabbitmq-http-auth-backend'),
 });
 
 export const expressLogger = expressWinston.logger({
   transports: [new winston.transports.Console()],
-  format: winston.format.combine(winston.format.json()),
+  format: winstonFormat('express'),
   meta: true,
   expressFormat: true,
-  colorize: process.env.NODE_ENV === 'production' ? false : true,
+  colorize: isProduction ? false : true,
 });
 
 export const expressErrorLogger = expressWinston.errorLogger({
   transports: [new winston.transports.Console()],
-  format: winston.format.combine(winston.format.json()),
+  format: winstonFormat('express-error'),
 });
