1d14754ad76344e6e55ed1d8209b24ba3a8d51e7
garyparrot <weedbug666@gmail.com>
2020-09-13 12:20:38 +0800

    feat: update vhost login

--- src/auth/vhost.ts
+++ src/auth/vhost.ts
@@ -1,8 +1,20 @@
+import {appLogger} from '../logger';
+
 export interface VhostRequestParam {
   username: string;
   vhost: string;
   ip: string;
 }
 export function authVhost({username, vhost, ip}: VhostRequestParam): boolean {
-  return vhost === 'user';
+  if (vhost === 'user') return true;
+  const [name, name_vhost] = username.split(':');
+  if (typeof name !== 'string' || typeof vhost !== 'string') {
+    appLogger.debug('Failed to claim vhost , bad username - ' + username);
+    return false;
+  }
+  if (vhost !== name_vhost) {
+    appLogger.debug('Failed to claim vhost, vhost not match');
+    return false;
+  }
+  return true;
 }
